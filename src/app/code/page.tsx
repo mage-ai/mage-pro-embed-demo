2dca0b09f2607cfc560be26352515a711fadd581
Tommy Dang <quantumventuress@gmail.com>
2025-03-25 18:02:41 -0700

    [td] Update example

--- src/app/code/page.tsx
+++ src/app/code/page.tsx
@@ -26,12 +26,10 @@ export default function Code() {
         >
           <svg dangerouslySetInnerHTML={{
             __html: `
-<svg xmlns="http://www.w3.org/2000/svg" width="1585" height="1200" viewBox="0 0 1585 1200" fill="none">
-  <path d="M959.066 0.000156824L625.312 0L0 1200L335.625 1200L623.437 647.679V1200H959.062V0.00718452L959.066 0.000156824Z" fill="#0500FF"/>
-  <path d="M1248.75 0.000220868H1584.37V1200H1248.75V0.000220868Z" fill="#0500FF"/>
-</svg>
+<path d="M959.066 0.000156824L625.312 0L0 1200L335.625 1200L623.437 647.679V1200H959.062V0.00718452L959.066 0.000156824Z" fill="#0500FF"/>
+<path d="M1248.75 0.000220868H1584.37V1200H1248.75V0.000220868Z" fill="#0500FF"/>
             `,
-          }} data-name="Ebene 1" height={24} id="Ebene_1" viewBox="0 0 433 97" width={24 * 433 / 97}
+          }} height={24} viewBox="0 0 1585 1200" width={32}
           />
           <Headline>Your logo and title</Headline>
         </Grid>
